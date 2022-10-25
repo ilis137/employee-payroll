ca2f0b44ab50866a14606e41d1e5038660b12cf3
ilis137 <iliyasshahapure1@gmail.com>
2022-10-25 18:35:14 +0530

    add:retrive data from employee payroll table  filtered by date and name

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -22,3 +22,7 @@ insert into employee_payroll_table
  (name,salary,start_date) values("Bill",10000,"2018-01-03"),("Teresa",20000,"2019-11-13"),("Charlie",30000,"2020-05-21");
  
  select * from employee_payroll_table;
+ 
+ select * from employee_payroll_table where name='Bill';
+ 
+ select * from employee_payroll_table where start_date between cast('2018-01-01' as date) and Date(now());
