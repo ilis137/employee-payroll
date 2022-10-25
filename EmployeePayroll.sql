803ec75c233c09e6df6ae09afab6394826cb1279
ilis137 <iliyasshahapure1@gmail.com>
2022-10-25 12:37:57 +0530

    add:create table for employee payroll

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -5,3 +5,15 @@ create database employee_payroll;
 use employee_payroll;
 
 select database();
+
+show tables;
+
+create table employee_payroll_table(
+id int unsigned auto_increment not null,
+name varchar(40),
+salary double,
+start_date date ,
+primary key(id)
+);
+
+describe employee_payroll_table;
